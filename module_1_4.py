16808e2490ef5d3ef6c8b02e8ea7d0c1365788ec
OlegTerek <olegter549@gmail.com>
2024-10-04 23:37:50 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,17 @@
+my_string = input("Введите ваше имя: ") #создаю переменную
+print ("ваше имя состоит из :",len(my_string),"символов.") #Вывожу количество символов введённого имени
+print(my_string.upper())   #вывод имени в верхнем регистре
+print(my_string.lower())   #вывод имени в нижнем регистре
+print(my_string.replace('',' '))  #вывод имени с  пробелами
+print(my_string.replace(' ',''))  #вывод имени без пробелов
+print(my_string[0])
+print(my_string[-1])
+
+
+
+
+
+
+
+
+
